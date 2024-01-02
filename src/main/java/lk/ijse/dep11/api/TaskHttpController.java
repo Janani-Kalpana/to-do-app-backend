c38490d546f306b712863f7a22d8f2c30fbc3d2d
Janani Kalpana <janani.kalpana1997@gmail.com>
2024-01-03 00:09:35 +0530

    implement createTask

--- src/main/java/lk/ijse/dep11/api/TaskHttpController.java
+++ src/main/java/lk/ijse/dep11/api/TaskHttpController.java
@@ -2,11 +2,13 @@ package lk.ijse.dep11.api;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.dep11.to.TaskTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PreDestroy;
+import java.sql.*;
 
 @RestController
 @RequestMapping("/api/v1/tasks")
@@ -28,4 +30,26 @@ public class TaskHttpController {
     public void destroy() {
         pool.close();
     }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public TaskTO createTask(@RequestBody @Validated TaskTO task) {
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection
+                    .prepareStatement("INSERT INTO task (description, status, email) VALUES (?, FALSE, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, task.getDescription());
+            stm.setString(2, task.getEmail());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            task.setId(id);
+            task.setStatus(false);
+            return task;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
